d911b8cfaa71e60d10e1ec3410bcb78f80d45537
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-04-25 23:24:43 +0100

    feat: adds global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,28 @@
+'use client';
+
+import { styled } from "@/styles";
+
+import { globalStyles } from '@/styles/global';
+
+globalStyles();
+
+const Button = styled('button', {
+  backgroundColor: '$green500',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold'
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  },
+})
 
 export default function Home() {
   return (
-    <h1>SHOP</h1>
+    <Button>Send</Button>
   );
 }
