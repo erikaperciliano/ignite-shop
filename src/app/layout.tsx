b1d1d4636ae3cf32052e4057eeef1aa8d0f4c3e5
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-05-05 20:00:57 +0100

    feat: adds structures of Product Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 'use client';
 
+import { globalStyles } from '@/styles/global';
 import { Header, Container } from '@/styles/pages/app';
 import logoImg from '../assets/logo.svg';
 import Image from 'next/image.js';
 
+globalStyles(); 
 export default function RootLayout({
   children,
 }: Readonly<{
--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,13 +1,24 @@
-export const revalidate = 60 * 60 * 2; // every 2 hours
-interface ProductPageProps {
-    params: {
-      id: string;
-    };
-}
 
-export default async function Product({ params }: ProductPageProps) {
+'use client';
 
-    return(
-        <h1>Product { params.id }</h1>
-    )
+import { ImageContainer, ProductContainer, ProductDetails } from "../../../styles/pages/product"
+
+export default function Product() {
+  return (
+    <ProductContainer>
+      <ImageContainer>
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>T-Shirt X</h1>
+        <span>$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores aliquid rerum exercitationem facere a molestiae ut sed velit non mollitia? Officiis hic velit assumenda aspernatur nihil, sint sed laboriosam tempora?</p>
+
+        <button>
+         Buy Now
+        </button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
